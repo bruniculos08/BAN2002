a9063efdd9d8d7679082cef296f66dc165621ac5
Adilson_krischanski <krischanski.adilson@gmail.com>
2022-09-24 17:19:55 -0300

    concluded database, need check

--- Trabalho01/database.sql
+++ Trabalho01/database.sql
@@ -15,18 +15,65 @@ create table public.Emissao_de _nota(
 
 create table public.fornecedor(
     cnpj character varying(14) not null,
-    nome haracter varying(100) not null,
+    nome character varying(100) not null,
     primary key(cnpj)
 );
 
-create table public.componente();
+create table public.componente(
+    referencia integer not null,
+    tipo character varying(50) not null,
+    minimo_quant integer not null,
+    quant integer not null,
+    setor_armazenamento integer not null,
+    cnpj_principal character varying(14) not null,
+    primary key(referencia)
+);
 
-create table public.veiculo();
+create table public.veiculo(
+    chassi character varying(17) not null,
+    manual_automatico boolean not null, -- 0 manual 1 automatico
+    arcondicionado boolean not null,
+    vidro_travas boolean not null,
+    cod_dept integer not null,
+    primary key(chassi)
+    FOREIGN KEY (cod_dept) references public.departamento (cod_dept)
+);
 
-create table public.esta_na_lista();
+create table public.esta_na_lista(
+    cod_dept integer not null,
+    referencia integer not null,
+    primary key(referencia),
+    primary key(cod_dept)
+    FOREIGN KEY (referencia) references public.componente (referencia)
+    FOREIGN KEY (cod_dept) references public.departamento (cod_dept)
 
-create table public.contem();
+);
 
-create table public.fornece();
+create table public.contem(
+    referencia integer not null,
+    id_pedido integer not null,
+    primary key(referencia),
+    primary key (id_pedido),
+    FOREIGN KEY (referencia) references public.componente (referencia),
+    FOREIGN KEY (id_pedido) references public.pedido_de_compra (id)
 
-create table public.pedido_de_compra();
+);
+
+create table public.fornece(
+    referencia integer not null,
+    cnpj character varying(14) not null,
+    primary key (referencia),
+    primary key (cnpj),
+    FOREIGN KEY (referencia) references public.componente (referencia)
+    FOREIGN KEY (cnpj) references public.fornecedor (cnpj)
+    
+);
+
+create table public.pedido_de_compra(
+    id integer not null,
+    cod_nota integer not null,
+    cnpj character varying(14) not null,
+    valor numeric not null,
+    primary key (id) not null,
+    FOREIGN key(cnpj) references puclic.fornecedor cnpj
+);
