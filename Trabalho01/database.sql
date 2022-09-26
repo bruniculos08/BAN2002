5355181671e0dda1859437c8e9e9166226794497
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-09-25 21:17:07 -0300

    update

--- Trabalho01/database.sql
+++ Trabalho01/database.sql
@@ -1,25 +1,35 @@
-create table pulbic.departamento(
+﻿create table departamento(
     cod_dept integer not null,
     tipo character varying(50) not null,
     primary key(cod_dept)
 );
 
-create table public.Emissao_de _nota(
+create table Emissao_de_nota(
     cod_nota integer not null,
     cod_depto_comum integer not null,
     cod_depto_compra Integer not null,
-    primary key(cod_nota)
-    FOREIGN KEY (cod_depto_comum ) references public.departamento (cod_dept)
-    FOREIGN KEY (cod_depto_compra) references public.departamento (cod_dept)
+    primary key(cod_nota),
+    FOREIGN KEY (cod_depto_comum ) references departamento (cod_dept),
+    FOREIGN KEY (cod_depto_compra) references departamento (cod_dept)
 );
 
-create table public.fornecedor(
+create table fornecedor(
     cnpj character varying(14) not null,
     nome character varying(100) not null,
     primary key(cnpj)
 );
 
-create table public.componente(
+
+create table pedido_de_compra(
+    id integer not null,
+    cod_nota integer not null,
+    cnpj character varying(14) not null,
+    valor numeric not null,
+    primary key (id),
+    FOREIGN key(cnpj) references fornecedor (cnpj)
+);
+
+create table componente(
     referencia integer not null,
     tipo character varying(50) not null,
     minimo_quant integer not null,
@@ -29,51 +39,40 @@ create table public.componente(
     primary key(referencia)
 );
 
-create table public.veiculo(
+create table veiculo(
     chassi character varying(17) not null,
     manual_automatico boolean not null, -- 0 manual 1 automatico
     arcondicionado boolean not null,
     vidro_travas boolean not null,
     cod_dept integer not null,
-    primary key(chassi)
-    FOREIGN KEY (cod_dept) references public.departamento (cod_dept)
+    primary key(chassi),
+    FOREIGN KEY (cod_dept) references departamento (cod_dept)
 );
 
-create table public.esta_na_lista(
+create table esta_na_lista(
     cod_dept integer not null,
     referencia integer not null,
-    primary key(referencia),
-    primary key(cod_dept)
-    FOREIGN KEY (referencia) references public.componente (referencia)
-    FOREIGN KEY (cod_dept) references public.departamento (cod_dept)
+    primary key(referencia, cod_dept),
+    FOREIGN KEY (referencia) references componente (referencia),
+    FOREIGN KEY (cod_dept) references departamento (cod_dept)
 
 );
 
-create table public.contem(
+create table contem(
     referencia integer not null,
     id_pedido integer not null,
-    primary key(referencia),
-    primary key (id_pedido),
-    FOREIGN KEY (referencia) references public.componente (referencia),
-    FOREIGN KEY (id_pedido) references public.pedido_de_compra (id)
+    primary key(referencia, id_pedido),
+    FOREIGN KEY (referencia) references componente (referencia),
+    FOREIGN KEY (id_pedido) references pedido_de_compra (id)
 
 );
 
-create table public.fornece(
+create table fornece(
     referencia integer not null,
     cnpj character varying(14) not null,
-    primary key (referencia),
-    primary key (cnpj),
-    FOREIGN KEY (referencia) references public.componente (referencia)
-    FOREIGN KEY (cnpj) references public.fornecedor (cnpj)
+    primary key (referencia, cnpj),
+    FOREIGN KEY (referencia) references componente (referencia),
+    FOREIGN KEY (cnpj) references fornecedor (cnpj)
     
 );
 
-create table public.pedido_de_compra(
-    id integer not null,
-    cod_nota integer not null,
-    cnpj character varying(14) not null,
-    valor numeric not null,
-    primary key (id) not null,
-    FOREIGN key(cnpj) references puclic.fornecedor cnpj
-);
