74211a2a820a6ad0e20368bc9fea209c4f4dca09
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-11-10 20:03:09 -0300

    update

--- Trabalho01/AppPython/View.py
+++ Trabalho01/AppPython/View.py
@@ -9,34 +9,39 @@ from PIL import ImageTk, Image
 # Aqui ficarão as classes de interface:
 class MenuPrincipal():
     # Objeto da classe principal do menu:
-    tela = Tk()
+    __tela = Tk()
     # Objeto da classe de frame que será usado para as caixas de entrada:
-    fieldBoxFrame = None
+    __fieldBoxFrame = None
     # Como poderá haver mais de uma caixa de entrada devemos ter um vetor para acessá-las:
-    fieldBoxes = None
+    __fieldBoxes = None
     # Objeto da classe da caixa texto, onde serão exibidos os resultados:
-    textBox = None
+    __textBox = None
 
     def __init__(self):
         self.colocarImagem()
         self.criarBotoesDeAba()
-        self.criarCamposDeInsercao()
+        # self.criarCamposDeInsercao(2)
+        # self.deletarCamposDeInsercao()
+        self.criarCamposDeInsercao(4)
         self.createTextBox()
         self.setCampoDeExibicao("Texto...")
         self.ajustarTela()
+        return
 
     def run(self):
-        self.tela.mainloop()
+        self.__tela.mainloop()
+        return
 
     def ajustarTela(self):
-        self.tela.geometry("1280x720")
-        self.tela.minsize(width=1280, height=720)
-        self.tela.maxsize(width=1280, height=720)
-        self.tela.title("Fábrica de veículos")
+        self.__tela.geometry("1280x720")
+        self.__tela.minsize(width=1280, height=720)
+        self.__tela.maxsize(width=1280, height=720)
+        self.__tela.title("Fábrica de veículos")
+        return
 
     def colocarImagem(self):
         # Criando frame para colocar a label contendo a imagem:
-        frame = Frame(self.tela, width=450, height=240)
+        frame = Frame(self.__tela, width=450, height=240)
         frame.pack()
         frame.place(anchor="center", relx=0.5, rely=0.5)
 
@@ -50,6 +55,7 @@ class MenuPrincipal():
         # Criando label para colocar a imagem:
         label_para_imagem = Label(frame, image = self.imagem_de_menu)
         label_para_imagem.pack()
+        return
         
     def criarBotoesDeAba(self):
 
@@ -60,35 +66,36 @@ class MenuPrincipal():
         # Criando lista de botões de aba (comands para uma cascade)
             
         # Botão para opções relacionadas a departamentos:
-        listaDeBotoesDepartamento = Menu(self.tela, tearoff=0)
+        listaDeBotoesDepartamento = Menu(self.__tela, tearoff=0)
         listaDeBotoesDepartamento.add_command(label = "Adicionar departamento", command = lambda : botaoFuncionando("adicionar departamento comum"))
         listaDeBotoesDepartamento.add_command(label = "Adicionar comprador", command = lambda : botaoFuncionando("adicionar departamento de compras"))
         
         # Botão para opções relacionadas à veículos:
-        listaDeBotoesVeiculo = Menu(self.tela, tearoff=0)
+        listaDeBotoesVeiculo = Menu(self.__tela, tearoff=0)
         listaDeBotoesVeiculo.add_command(label = "Adicionar", command = lambda : botaoFuncionando("adicionar veículo"))
         listaDeBotoesVeiculo.add_command(label = "Remover", command = lambda : botaoFuncionando("remover veículo"))
         
         # Botão para opções relacionadas à pedidos:
-        listaDeBotoesPedido = Menu(self.tela, tearoff=0)
+        listaDeBotoesPedido = Menu(self.__tela, tearoff=0)
         listaDeBotoesPedido.add_command(label = "Adicionar", command = lambda : botaoFuncionando("adicionar pedido de compra"))
         listaDeBotoesPedido.add_command(label = "Ver lista", command = lambda : botaoFuncionando("ver lista de pedidos"))
         listaDeBotoesPedido.add_command(label = "Remover", command = lambda : botaoFuncionando("Remover pedido de compra"))
 
         # Botão para opções relacionadas à componentes:
-        listaDeBotoesComponente = Menu(self.tela, tearoff=0)
+        listaDeBotoesComponente = Menu(self.__tela, tearoff=0)
         listaDeBotoesComponente.add_command(label = "Adicionar", command = lambda : botaoFuncionando("adicionar componente"))
         listaDeBotoesComponente.add_command(label = "Remover", command = lambda : botaoFuncionando("remover componente"))
         
         # Criando barra superior de botões de aba:
-        listaDelistaDeBotoes = Menu(self.tela)
+        listaDelistaDeBotoes = Menu(self.__tela)
         listaDelistaDeBotoes.add_cascade(label = "Departamentos", menu = listaDeBotoesDepartamento)
         listaDelistaDeBotoes.add_cascade(label = "Veículos", menu = listaDeBotoesVeiculo)
         listaDelistaDeBotoes.add_cascade(label = "Pedidos", menu = listaDeBotoesPedido)
         listaDelistaDeBotoes.add_cascade(label = "Componentes", menu = listaDeBotoesComponente)
 
         # Adicionando a lista de listas de botões criados à tela:
-        self.tela.config(menu = listaDelistaDeBotoes)
+        self.__tela.config(menu = listaDelistaDeBotoes)
+        return
     
     # Abrir campo de inserção:
     def setOnCampoDeInsercao(self, string):
@@ -99,7 +106,7 @@ class MenuPrincipal():
         
         # Criando frame para colocar a fieldBox:
         fieldBoxFrameColor = 'purple'
-        fieldBoxFrame = Frame(self.tela, bd = 2, width=400, height=80, bg = fieldBoxFrameColor, relief=SOLID)
+        fieldBoxFrame = Frame(self.__tela, bd = 2, width=400, height=80, bg = fieldBoxFrameColor, relief=SOLID)
         fieldBoxFrame.pack()
         fieldBoxFrame.place(anchor="center", relx=0.2, rely=0.50)
 
@@ -110,27 +117,29 @@ class MenuPrincipal():
         # Criando a fieldBox:
         self.fieldBox = Entry(fieldBoxFrame, width=61, borderwidth=2, bg = 'white', relief=SOLID)
         self.fieldBox.place(anchor="center", relx=0.5, rely=0.5)
+        return
 
-    def criarCamposDeInsercao(self, quantidade=11):
-        self.fieldBoxFrame = Frame(self.tela, bd = 2, width=400, height=50*quantidade, bg = "purple", relief=SOLID)
-        self.fieldBoxFrame.pack()
-        self.fieldBoxFrame.place(anchor="center", relx=0.2, rely=0.50)
-        self.fieldBoxes = []
+    def criarCamposDeInsercao(self, quantidade):
+        quantidade = 15
+        self.__fieldBoxFrame = Frame(self.__tela, bd = 2, width=400, height=(46.5)*quantidade + 10, bg = "purple", relief=SOLID)
+        self.__fieldBoxFrame.pack()
+        self.__fieldBoxFrame.place(anchor="center", relx=0.2, rely=0.50)
+        self.__fieldBoxes = []
 
         # Adicionando campos de entradas:
         for i in range(0, 2*quantidade, 2):
-            fieldBoxLabel = Label(self.fieldBoxFrame, text = f"Campo {int(i/2)+1}:", height = 1, width = 10, bg = 'purple', font =("Courier", 10))
+            fieldBoxLabel = Label(self.__fieldBoxFrame, text = f"Campo {int(i/2)+1}:", height = 2, width = 10, bg = 'purple', font =("Courier", 10))
             fieldBoxLabel.pack()
-            fieldBoxLabel.place(anchor="n", x=200, y=(22)*(i + 1))
+            fieldBoxLabel.place(anchor="n", x=200, y=23*i + 0.1)
 
-            fieldBox = Entry(self.fieldBoxFrame, width=50, borderwidth=2, bg = 'white', relief=SOLID)
-            fieldBox.place(anchor="n", x=200, y=(22)*(i + 2))
-            self.fieldBoxes.append(fieldBox)
+            fieldBox = Entry(self.__fieldBoxFrame, width=50, borderwidth=2, bg = 'white', relief=SOLID)
+            fieldBox.place(anchor="n", x=200, y=23*(i+1.1) + 0.1)
+            self.__fieldBoxes.append(fieldBox)
         return
     
     def deletarCamposDeInsercao(self):
-        self.fieldBoxFrame.destroy()
-        for field in self.fieldBoxes:
+        self.__fieldBoxFrame.destroy()
+        for field in self.__fieldBoxes:
             field.destroy()
         return
 
@@ -141,12 +150,13 @@ class MenuPrincipal():
             return
         self.fieldBox.destroy()
         self.fieldBox = None
+        return
 
     def createTextBox(self):
         # Criando os objetos:
-        self.textBox = Text(self.tela, height=40, width=100, borderwidth=4, relief="solid")
-        self.textBox.config(font =("Courier", 8))
-        label = Label(self.tela, text = "Resultado", width=50, bg = "grey", borderwidth=6.4, relief="solid")
+        self.__textBox = Text(self.__tela, height=40, width=100, borderwidth=4, relief="solid")
+        self.__textBox.config(font =("Courier", 8))
+        label = Label(self.__tela, text = "Resultado", width=50, bg = "grey", borderwidth=6.4, relief="solid")
         label.config(font =("Courier", 12))
 
         # Packs e places do label:
@@ -154,13 +164,14 @@ class MenuPrincipal():
         label.place(anchor="center", relx=0.7, rely=0.1)
 
         # Packs e places da textBox:
-        self.textBox.pack()
-        self.textBox.place(anchor="n", relx=0.7, rely=0.14)
-        self.textBox.config(state=DISABLED)
+        self.__textBox.pack()
+        self.__textBox.place(anchor="n", relx=0.7, rely=0.14)
+        self.__textBox.config(state=DISABLED)
+        return
 
     def setCampoDeExibicao(self, string):
-        self.textBox.config(state=NORMAL)
-        self.textBox.delete('1.0', END)
-        self.textBox.insert(END, string)
-        self.textBox.config(state=DISABLED)
-        pass    
+        self.__textBox.config(state=NORMAL)
+        self.__textBox.delete('1.0', END)
+        self.__textBox.insert(END, string)
+        self.__textBox.config(state=DISABLED)
+        return    
